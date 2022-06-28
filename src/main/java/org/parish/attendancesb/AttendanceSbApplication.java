653ffb0db3f7699f2bcc91ec47688eff7b8d1652
christianvilca <christian.vilcaq@gmail.com>
2022-06-28 01:59:13 -0500

    Initial

---
+++ src/main/java/org/parish/attendancesb/AttendanceSbApplication.java
@@ -0,0 +1,13 @@
+package org.parish.attendancesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceSbApplication.class, args);
+	}
+
+}
