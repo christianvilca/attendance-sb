b03a4582281f9d7c6dd4c49c0e84edcc0520dd39
christianvilca <christian.vilcaq@gmail.com>
2022-08-06 22:53:22 -0500

    feat: implement AttendanceDateService

--- src/main/java/org/parish/attendancesb/services/AttendanceDateServiceImpl.java
+++ src/main/java/org/parish/attendancesb/services/AttendanceDateServiceImpl.java
@@ -1,6 +1,8 @@
 package org.parish.attendancesb.services;
 
 import org.parish.attendancesb.models.AttendanceDate;
+import org.parish.attendancesb.models.Catequesis;
+import org.parish.attendancesb.repositories.AttendanceDateRepository;
 import org.parish.attendancesb.services.interfaces.AttendanceDateService;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,48 @@ import java.util.List;
 
 @Service
 public class AttendanceDateServiceImpl implements AttendanceDateService {
-    @Override
-    public List<AttendanceDate> getAllByIdCatequesis(Integer idCatequesis) {
-        return null;
+
+    private AttendanceDateRepository repository;
+
+    public AttendanceDateServiceImpl(AttendanceDateRepository repository) {
+        this.repository = repository;
     }
+
+    @Override
+    public List<AttendanceDate> findAllByCatequesis(Catequesis catequesis) {
+        return repository.findAllByCatequesis(catequesis);
+    }
+
+    @Override
+    public AttendanceDate getById(Integer id) {
+        return repository.getById(id);
+    }
+
+    @Override
+    public AttendanceDate save(AttendanceDate attendanceDate) {
+        return repository.save(attendanceDate);
+    }
+
+    @Override
+    public void delete(AttendanceDate attendanceDate) {
+        repository.delete(attendanceDate);
+    }
+
+    @Override
+    public AttendanceDate update(AttendanceDate attendanceDate) {
+        return repository.findById(attendanceDate.getId())
+                .map(
+                        ad -> {
+                            ad.setCatequesis(attendanceDate.getCatequesis());
+                            ad.setDate(attendanceDate.getDate());
+                            return repository.save(ad);
+                        }
+                ).orElse(null);
+    }
+
+    @Override
+    public List<AttendanceDate> findAll() {
+        return repository.findAll();
+    }
+
 }
--- src/main/java/org/parish/attendancesb/services/interfaces/AttendanceDateService.java
+++ src/main/java/org/parish/attendancesb/services/interfaces/AttendanceDateService.java
@@ -1,9 +1,12 @@
 package org.parish.attendancesb.services.interfaces;
 
 import org.parish.attendancesb.models.AttendanceDate;
+import org.parish.attendancesb.models.Catequesis;
 
 import java.util.List;
 
-public interface AttendanceDateService {
-    public List<AttendanceDate> getAllByIdCatequesis(Integer idCatequesis);
+public interface AttendanceDateService extends Service<Integer, AttendanceDate> {
+
+    public List<AttendanceDate> findAllByCatequesis(Catequesis catequesis);
+
 }
