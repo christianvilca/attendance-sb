b4c038f2b9bf8ce32fd275925129e5defa0c4fed
christianvilca <christian.vilcaq@gmail.com>
2022-08-08 22:55:50 -0500

    feat: findByDate to AttendanceDateRepository

--- src/main/java/org/parish/attendancesb/repositories/AttendanceDateRepository.java
+++ src/main/java/org/parish/attendancesb/repositories/AttendanceDateRepository.java
@@ -2,6 +2,7 @@ package org.parish.attendancesb.repositories;
 
 import org.parish.attendancesb.models.AttendanceDate;
 import org.parish.attendancesb.models.Catequesis;
+import org.parish.attendancesb.models.datetime.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,7 @@ import java.util.List;
 public interface AttendanceDateRepository extends JpaRepository<AttendanceDate, Integer> {
 
     public List<AttendanceDate> findAllByCatequesis(Catequesis catequesis);
+
+    public AttendanceDate findByDate(Date date);
 
 }
