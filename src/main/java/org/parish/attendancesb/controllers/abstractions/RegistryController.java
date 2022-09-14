7f655e870e91e1a88f392531b970ce5d96cf466f
christianvilca <christian.vilcaq@gmail.com>
2022-09-14 12:56:38 -0500

    feat: create abstract class's to controllers

---
+++ src/main/java/org/parish/attendancesb/controllers/abstractions/RegistryController.java
@@ -0,0 +1,84 @@
+package org.parish.attendancesb.controllers.abstractions;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.parish.attendancesb.controllers.utils.Alert;
+import org.parish.attendancesb.services.interfaces.Service;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public abstract class RegistryController<T> implements Initializable {
+
+    protected T registry;
+
+    protected Service service;
+
+    protected RegistryController(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeObjects();
+
+        if (registry == null) {
+            clearFields();
+            return;
+        }
+
+        setFieldsFromModel();
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+        T registry = this.getModelFromFields();
+
+        if (this.service.contains(registry)) {
+            Alert.error("El registro ya existe!");
+            return;
+        }
+
+        saveRegistry(registry);
+
+        Alert.information("Se ha guardado correctamente!");
+
+        this.cancel(event);
+    }
+
+    private void saveRegistry(T registry){
+        if (this.registry == null) {
+            this.registry = registry;
+            this.service.save(registry);
+            return;
+        }
+
+        this.service.update(registry);
+    }
+
+    @FXML
+    void cancel(ActionEvent event) {
+        Button button = (Button) event.getSource();
+        Stage stage = (Stage) button.getScene().getWindow();
+        stage.close();
+    }
+
+    public T getModel() {
+        return this.registry;
+    }
+
+    public void setModel(T t) {
+        registry = t;
+    }
+
+    protected abstract void initializeObjects();
+
+    protected abstract T getModelFromFields();
+
+    protected abstract void setFieldsFromModel();
+
+    protected abstract void clearFields();
+}
---
+++ src/main/java/org/parish/attendancesb/controllers/abstractions/RegistryListController.java
@@ -0,0 +1,155 @@
+package org.parish.attendancesb.controllers.abstractions;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import net.sf.jasperreports.engine.JRException;
+import org.parish.attendancesb.config.StageManager;
+import org.parish.attendancesb.controllers.utils.Alert;
+import org.parish.attendancesb.report.Jrxml;
+import org.parish.attendancesb.report.Report;
+import org.parish.attendancesb.services.interfaces.Service;
+import org.parish.attendancesb.view.FxmlView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public abstract class RegistryListController<T> implements Initializable {
+
+    @FXML
+    private Label total;
+
+    @FXML
+    private TableView<T> table;
+
+    @FXML
+    private TextField search;
+
+    protected FxmlView view;
+
+    protected Jrxml report;
+
+    @Autowired
+    private Report reportService;
+
+    @Lazy
+    @Autowired
+    private StageManager stageManager;
+
+    protected RegistryController controller;
+
+    protected Service service;
+
+    public RegistryListController(RegistryController controller, Service service) {
+        this.controller = controller;
+        this.service = service;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                search.requestFocus();
+            }
+        });
+
+        this.initializeObjects();
+        this.refleshTable();
+        this.setColumnFromModel();
+        this.dblClickTable();
+    }
+
+    public void dblClickTable() {
+        this.table.setRowFactory(tv -> {
+            TableRow<T> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    T rowData = row.getItem();
+                    this.edit(null);
+                }
+            });
+            return row;
+        });
+    }
+
+    protected void setModal(FxmlView view) {
+        this.view = view;
+    }
+
+    protected void setReport(Jrxml report) {
+        this.report = report;
+    }
+
+    @FXML
+    void export(ActionEvent event) {
+        try {
+            reportService.export(this.table.getItems(), report);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (JRException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void showModal() {
+        stageManager.sceneModal(view);
+
+        if (controller.getModel() != null)
+            this.refleshTable();
+    }
+
+    @FXML
+    void newRegistry(ActionEvent event) {
+        controller.setModel(null);
+        showModal();
+    }
+
+    @FXML
+    void filter(KeyEvent event) {
+        refleshTable();
+    }
+
+    @FXML
+    void edit(ActionEvent event) {
+        T registry = this.table.getSelectionModel().getSelectedItem();
+
+        if (registry == null) {
+            Alert.error("Debe seleccionar un registro!");
+            return;
+        }
+
+        controller.setModel(registry);
+        showModal();
+    }
+
+    public void refleshTable() {
+        String textSearch = this.search.getText().trim();
+
+        List<T> people;
+        if (textSearch.isEmpty())
+            people = this.service.findAll();
+        else
+            people = this.service.findByName(textSearch);
+
+        this.table.setItems(FXCollections.observableArrayList(people));
+        this.table.refresh();
+
+        this.total.setText(people.size() + "");
+    }
+
+    protected abstract void initializeObjects();
+
+    protected abstract void setColumnFromModel();
+}
