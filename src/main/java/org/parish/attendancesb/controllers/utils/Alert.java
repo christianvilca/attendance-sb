2a304f3cfca356979224586e68e272970ba79a6f
christianvilca <christian.vilcaq@gmail.com>
2022-11-16 14:11:20 -0500

    feat: add method yesno in Alert

--- src/main/java/org/parish/attendancesb/controllers/utils/Alert.java
+++ src/main/java/org/parish/attendancesb/controllers/utils/Alert.java
@@ -1,5 +1,10 @@
 package org.parish.attendancesb.controllers.utils;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
 public class Alert {
 
     private Alert() {
@@ -33,6 +38,27 @@ public class Alert {
         alert.setTitle(WARNING);
         alert.setContentText(description);
         alert.showAndWait();
+    }
+
+    public static boolean yesno(String description) {
+        final String WARNING = "Warning";
+
+        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.WARNING);
+        alert.setHeaderText(null);
+        alert.setTitle(WARNING);
+        alert.setContentText(description);
+
+        alert.getButtonTypes().clear();
+        alert.getButtonTypes().addAll(ButtonType.YES, ButtonType.NO);
+
+        Button yesButton = (Button) alert.getDialogPane().lookupButton( ButtonType.YES );
+        yesButton.setDefaultButton( false );
+
+        Button noButton = (Button) alert.getDialogPane().lookupButton( ButtonType.NO );
+        noButton.setDefaultButton( true );
+
+        final Optional<ButtonType> result = alert.showAndWait();
+        return result.get() == ButtonType.YES;
     }
 
 }
