5e5e9043abf14b531aa5b77359a3eaafa2887fbf
christianvilca <christian.vilcaq@gmail.com>
2022-11-25 14:27:12 -0500

    feat: add menu Catequistas in GroupListController

--- src/main/java/org/parish/attendancesb/controllers/GroupListController.java
+++ src/main/java/org/parish/attendancesb/controllers/GroupListController.java
@@ -1,13 +1,17 @@
 package org.parish.attendancesb.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.parish.attendancesb.config.StageManager;
 import org.parish.attendancesb.controllers.abstractions.RegistryListController;
 import org.parish.attendancesb.models.Group;
 import org.parish.attendancesb.report.Jrxml;
 import org.parish.attendancesb.services.interfaces.GroupService;
 import org.parish.attendancesb.view.FxmlView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -22,14 +26,42 @@ public class GroupListController extends RegistryListController<Group> {
     @FXML
     private TableColumn<?, ?> name;
 
-    public GroupListController(GroupController controller, GroupService service) {
+    @Lazy
+    @Autowired
+    private StageManager stageManager;
+
+    private GroupCatequistaListController groupCatequistaListController;
+
+    public GroupListController(
+            GroupController controller,
+            GroupService service,
+            GroupCatequistaListController groupCatequistaListController
+    ) {
         super(controller, service);
+        this.groupCatequistaListController = groupCatequistaListController;
     }
 
     @Override
     public void initializeObjects() {
         setModal(FxmlView.GROUP);
         setReport(Jrxml.GROUP_LIST);
+        this.addAllMenu();
+    }
+
+    private void addAllMenu() {
+        addAllMenu(
+                new SeparatorMenuItem(),
+                getMenuCatequistas()
+        );
+    }
+
+    private MenuItem getMenuCatequistas() {
+        MenuItem menu = new MenuItem("Catequistas");
+        menu.setOnAction((ActionEvent e) -> {
+            groupCatequistaListController.setGroup(getRow());
+            stageManager.sceneModal(FxmlView.GROUP_CATEQUUISTA_LIST);
+        });
+        return menu;
     }
 
     @Override
