ec2ed75a98fb6da91a92dcacafab72793ef1255f
christianvilca <christian.vilcaq@gmail.com>
2022-12-30 21:12:31 -0500

    refactor: ImageBase64

--- src/main/java/org/parish/attendancesb/models/image/ImageBase64.java
+++ src/main/java/org/parish/attendancesb/models/image/ImageBase64.java
@@ -12,101 +12,61 @@ import java.util.Base64;
 public class ImageBase64 {
 
     public static String encoder(Image fxImage) {
-        ByteArrayOutputStream outputStream = null;
         try {
             if (fxImage != null) {
-                outputStream = new ByteArrayOutputStream();
-                BufferedImage bufferedImage = SwingFXUtils.fromFXImage(fxImage, null);
-//                ImageIO.write(bufferedImage, getFileExtension(fxImage.getUrl()), outputStream);
-                ImageIO.write(bufferedImage, "png", outputStream);
+                return encoder(SwingFXUtils.fromFXImage(fxImage, null));
             }
         } catch (IOException e) {
             Alert.error(e.getMessage());
-        }
-
-        if (outputStream == null) {
-            return null;
-        }
-
-        byte[] imageBytes = outputStream.toByteArray();
-        return Base64.getEncoder().encodeToString(imageBytes);
-    }
-
-    public static String encoder(BufferedImage bufferedImage) {
-        ByteArrayOutputStream outputStream = null;
-        try {
-            if (bufferedImage != null) {
-                outputStream = new ByteArrayOutputStream();
-                ImageIO.write(bufferedImage, "png", outputStream);
-            }
-        } catch (IOException e) {
-            Alert.error(e.getMessage());
-        }
-
-        if (outputStream == null) {
-            return null;
-        }
-
-        byte[] imageBytes = outputStream.toByteArray();
-        return Base64.getEncoder().encodeToString(imageBytes);
-    }
-
-    private static String getFileExtension(String fileUri) {
-        return fileUri.split("\\.")[1];
-    }
-
-    public static Image decoder(String imageBase64) {
-        if (imageBase64 != null) {
-            // Decodifica la cadena y obtiene un arreglo de bytes
-            byte[] imageBytes = Base64.getDecoder().decode(imageBase64);
-
-            return new Image(new ByteArrayInputStream(imageBytes));
         }
 
         return null;
     }
 
-    public static BufferedImage decodeBuffer(String imageBase64) {
-        BufferedImage image = null;
+    public static String encoder(BufferedImage bufferedImage) throws IOException {
+        ByteArrayOutputStream outputStream = null;
 
-        if (imageBase64 != null) {
-            // Decodifica la cadena y obtiene un arreglo de bytes
-            byte[] imageBytes = Base64.getDecoder().decode(imageBase64);
-
-            ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
-            try {
-                image = ImageIO.read(bis);
-                bis.close();
-            } catch (IOException e) {
-                Alert.error(e.getMessage());
-            }
+        if (bufferedImage != null) {
+            outputStream = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "png", outputStream);
         }
 
-        return image;
-    }
+        if (outputStream == null) {
+            return null;
+        }
 
-    public static String encoderFromPath(String path) throws IOException {
-        // Abre la imagen como un stream de entrada
-        FileInputStream imageInputStream = new FileInputStream(path);
-        // Crea un arreglo de bytes para almacenar la imagen
-        byte[] imageBytes = new byte[imageInputStream.available()];
-        // Lee la imagen en el arreglo de bytes
-        imageInputStream.read(imageBytes);
-        // Cierra el stream de entrada
-        imageInputStream.close();
-
-        // Codifica el arreglo de bytes en una cadena en base64
+        byte[] imageBytes = outputStream.toByteArray();
         return Base64.getEncoder().encodeToString(imageBytes);
     }
 
-    public static void decoderToFile(String imageBase64) throws IOException {
+    public static Image decoder(String imageBase64) {
+        if (imageBase64 == null)
+            return null;
+
+        return new Image(toBais(imageBase64));
+    }
+
+    private static ByteArrayInputStream toBais(String imageBase64) {
         // Decodifica la cadena y obtiene un arreglo de bytes
         byte[] imageBytes = Base64.getDecoder().decode(imageBase64);
-        // Abre un stream de salida para escribir la imagen en un archivo
-        FileOutputStream imageOutputStream = new FileOutputStream("image.png");
-        // Escribe el arreglo de bytes en el stream de salida
-        imageOutputStream.write(imageBytes);
-        // Cierra el stream de salida
-        imageOutputStream.close();
+
+        return new ByteArrayInputStream(imageBytes);
+    }
+
+    public static BufferedImage decodeToBufferImage(String imageBase64) {
+        if (imageBase64 == null)
+            return null;
+
+        BufferedImage image = null;
+        ByteArrayInputStream bis = toBais(imageBase64);
+
+        try {
+            image = ImageIO.read(bis);
+            bis.close();
+        } catch (IOException e) {
+            Alert.error(e.getMessage());
+        }
+
+        return image;
     }
 }
